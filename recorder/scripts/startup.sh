d0963583a4ce64d5eb44513ebfe9240d94b0a678
Ole <ole@nocarrier.de>
2020-12-05 14:56:14 +0100

    Fixed startup script

--- recorder/scripts/startup.sh
+++ recorder/scripts/startup.sh
@@ -1,15 +1,15 @@
-time()
+ts()
 {
  date +"%Y-%m-%d %T"
 }
 
 
-cd ./home/pi/recorder/scripts
-echo "$(time): Power on. Initiatlizing" >>/var/log/mcm.log
+cd /home/pi/recorder/scripts
+echo "$(ts) Power on. Initiatlizing" >>/var/log/mcm.log
 
-python pwrblink.py
+python pwrblink.py &
 sleep 10
 pkill -f pwrblink.py
 python ./pwron.py
 
-echo "$(time): Ready to go" >>/var/log/mcm.log
+echo "$(ts) Ready to go" >>/var/log/mcm.log
