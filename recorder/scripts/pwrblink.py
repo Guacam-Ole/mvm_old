9b6213c57d40fb732a240edb637a3b3e475cd1ca
Ole <ole@nocarrier.de>
2020-12-05 13:14:38 +0100

    infinite loop for blink

--- recorder/scripts/pwrblink.py
+++ recorder/scripts/pwrblink.py
@@ -4,7 +4,7 @@ import time
 GPIO.setmode(GPIO.BCM) 
 GPIO.setwarnings(False) 
 GPIO.setup(14,GPIO.OUT) 
-for x in range(0, 5): 
+while(true)
     GPIO.output(14,GPIO.HIGH) 
     time.sleep(1)
     GPIO.output(14,GPIO.LOW) 
