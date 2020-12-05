b61ef2db0c97781fe1d02285d58f3f276885f4d2
Ole on pi <mvm.pi@oles.cloud>
2020-12-05 17:18:54 +0100

    update permssions

--- recorder/scripts/recbut.py
+++ recorder/scripts/recbut.py
@@ -8,8 +8,7 @@ def rec_callback(channel):
 GPIO.setmode(GPIO.BCM) 
 GPIO.setwarnings(False) 
 GPIO.setup(27,GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
-GPIO.add_event_detect (10, GPIO.RISING, callback=rec_callback)
+GPIO.add_event_detect (27, GPIO.RISING, callback=rec_callback)
 
-
-
-
+while (True):
+	time.sleep(1)
